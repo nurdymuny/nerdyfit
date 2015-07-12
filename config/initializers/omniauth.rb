5528ad567830681e9bda4168b9c6f172d2de9b25
Brian Russel Davis <brian@nurdymuny.com>
2015-07-12 17:24:19 +0545

    Requirement JSON output fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  ENV["GOOGLE_CLIENT_ID"] = 'xxx'
-  ENV["GOOGLE_SECRET"] = 'xxx'
+  ENV["GOOGLE_CLIENT_ID"] = '1071864229687-bi61a02ongoe0r8e0o0qh4ms1bg7ocr9.apps.googleusercontent.com'
+  ENV["GOOGLE_SECRET"] = 'aaB7teOKllQAXu5abJEY2SM-'
 
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
         scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/fitness.activity.read', 'https://www.googleapis.com/auth/fitness.body.read'], access_type: 'offline', name: 'google'
